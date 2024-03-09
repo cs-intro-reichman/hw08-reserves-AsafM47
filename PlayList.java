c6c752ea8ab693690ff1a6a404fb402124bbddef
asafm47 <asaf.manor@post.runi.ac.il>
2024-03-09 21:45:44 +0200

    First

--- PlayList.java
+++ PlayList.java
@@ -35,33 +35,58 @@ class PlayList {
      *  If the list is full, does nothing and returns false.
      *  Otherwise, appends the track and returns true. */
     public boolean add(Track track) {
-        //// replace the following statement with your code
-        return true;
+        if(this.size<this.maxSize){
+            tracks[size] = track;
+            size++;
+            return true; 
+        }
+        return false;
     }
 
     /** Returns the data of this list, as a string. Each track appears in a separate line. */
     //// For an efficient implementation, use StringBuilder.
     public String toString() {
-        //// replace the following statement with your code
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<size; i++){
+            String trck = this.getTrack(i).toString();
+            sb.append(trck);
+            sb.append('\n');
+        }
+        String finalString = sb.toString();
+        return finalString;
     }
 
     /** Removes the last track from this list. If the list is empty, does nothing. */
      public void removeLast() {
-        //// replace this comment with your code
+        if(size>0){
+            this.tracks[size-1] = null;
+            size--; 
+        }
     }
     
     /** Returns the total duration (in seconds) of all the tracks in this list.*/
     public int totalDuration() {
-        //// replace the following statement with your code
-        return 0;
+        int totalDuration = 0;
+        for(int i=0; i<size; i++){
+            totalDuration += this.tracks[i].getDuration();
+        }
+        return totalDuration;
     }
 
     /** Returns the index of the track with the given title in this list.
      *  If such a track is not found, returns -1. */
     public int indexOf(String title) {
-        //// replace the following statement with your code
-        return -1;
+        int index = -1; 
+        String track = ""; 
+        for(int i =0; i<size; i++){
+            track = this.tracks[i].getTitle(); // set track string to the title of the current track we are checking. 
+            if(track.equals(title)){
+                index = i;
+            }
+        }
+
+
+        return index;
     }
 
     /** Inserts the given track in index i of this list. For example, if the list is
@@ -71,34 +96,59 @@ class PlayList {
      *  is full, does nothing and returns false. Otherwise, inserts the track and
      *  returns true. */
     public boolean add(int i, Track track) {
-        //// replace the following statement with your code
-        return false;
+        if(i<0 || i>size || size == maxSize){
+            return false;
+        }
+        for (int j = size; j > i; j--) {
+            tracks[j] = tracks[j - 1];
+        }
+        tracks[i] = track;
+        size++;
+        return true;   
     }
      
     /** Removes the track in the given index from this list.
      *  If the list is empty, or the given index is negative or too big for this list, 
      *  does nothing and returns -1. */
     public void remove(int i) {
-        //// replace this comment with your code
+        if(size == 0 || i< 0 || i>= size){
+            return;
+        }
+        for(int j = i; j<size-1;j++){
+            tracks[j] = tracks[j+1];
+        }
+        tracks[size-1] = null;
+        size--;
+
     }
 
     /** Removes the first track that has the given title from this list.
      *  If such a track is not found, or the list is empty, or the given index
      *  is negative or too big for this list, does nothing. */
     public void remove(String title) {
-        //// replace this comment with your code
+        int inex = indexOf(title);
+        remove(inex);
     }
 
     /** Removes the first track from this list. If the list is empty, does nothing. */
     public void removeFirst() {
-        //// replace this comment with your code
+        if(size != 0);
+        {
+            remove(1);
+        }
+        
     }
     
     /** Adds all the tracks in the other list to the end of this list. 
      *  If the total size of both lists is too large, does nothing. */
     //// An elegant and terribly inefficient implementation.
      public void add(PlayList other) {
-        //// replace this comment with your code
+        if(this.size + other.getSize() > this.maxSize){
+            return;
+        }
+        for(int i =0; i<other.getSize(); i++){
+            this.add(other.getTrack(i));
+        }
     }
 
     /** Returns the index in this list of the track that has the shortest duration,
@@ -108,8 +158,20 @@ class PlayList {
      *  If start is negative or greater than size - 1, returns -1.
      */
     private int minIndex(int start) {
-        //// replace the following statement with your code
-        return 0;
+        if(start<0 || start>size-1){
+            return -1;
+        }
+        int temp = this.tracks[start].getDuration();
+        int min = temp;
+        int index = start; 
+        for(int i = start; i< size; i++){
+            temp = this.tracks[i].getDuration();
+            if(temp < min){
+                min = temp;
+                index = i;
+            }
+        }
+        return index;
     }
 
     /** Returns the title of the shortest track in this list. 
@@ -126,5 +188,13 @@ class PlayList {
         // Uses the selection sort algorithm,  
         // calling the minIndex method in each iteration.
         //// replace this statement with your code
+        Track temp = null;
+        int min = -1; 
+        for(int i =0; i<size; i++){
+            min = minIndex(i);
+            temp = this.tracks[min];
+            this.tracks[min] = this.tracks[i];
+            this.tracks[i] = temp;
+        }
     }
 }
--- Track.java
+++ Track.java
@@ -42,7 +42,10 @@ class Track {
     // Returns a string that represents the totalSeconds as "minutes:seconds",
     // Where seconds is always two digits. For example, "3:17" or "12:05".
     private String formattedDuration(int totalSeconds) {
-        //// replace the following statement with your code
-        return "";
-    }
+        int minutes = totalSeconds/60;
+        int seconds = totalSeconds%60;
+
+            String duration = String.format("\"%d:0%02d", minutes,seconds);
+            return duration;
+        }
 }
